b3026a4515f831c4f296c84d5c71b3608de12fb7
fantasykill <1872385668@qq.com>
2018-02-12 15:29:24 +0800

    init project

---
+++ src/main/java/com/wyf/bootdemo/BootdemoApplication.java
@@ -0,0 +1,12 @@
+package com.wyf.bootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootdemoApplication.class, args);
+	}
+}
