7820ff1ebe486bdd966dc8baf091a24f54c72de8
vvyun <1872385668@qq.com>
2019-11-12 16:59:00 +0800

    del log test

--- src/main/java/com/wyf/controller/IndexController.java
+++ src/main/java/com/wyf/controller/IndexController.java
@@ -1,12 +1,7 @@
 package com.wyf.controller;
 
-import com.wyf.entity.MLog;
-import com.wyf.service.MLogService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 /**
  * @author Yunfei
@@ -14,28 +9,8 @@ import java.util.List;
 @Controller
 public class IndexController {
 
-    @Autowired
-    MLogService logService;
-
     @RequestMapping("/")
     public String indexPage() {
-
-//        for (int i = 0; i < 2 ; i++) {
-//            MLog newLog = new MLog();
-//            newLog.setFlag(1);
-//            newLog.setType(1);
-//            newLog.setRequestContent("{id:"+i+"}");
-//            newLog.setResponseContent("{name:wyf"+i+"}");
-//            logService.add(newLog);
-//        }
-
-//        List list = logService.getAllLogs();
-//        List list = logService.listAllLog();
-//
-//        for (Object t : list) {
-//            MLog log = (MLog) t;
-//            System.out.println(log.toString());
-//        }
 
         return "index";
     }
